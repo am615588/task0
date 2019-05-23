d35152746228c986157e90be1bf99b18598420ab
sridatha <sridathatumati@gmail.com>
2019-05-23 06:20:08 -0700

    this is my first task0

---
+++ task1.py
@@ -0,0 +1,21 @@
+n=100
+counter = NULL
+counter.output = NULL
+counter = seq(1, n, by=1)
+
+a=3
+b=5
+
+for (i in 1:n){
+  if(counter[i] %% a == 0 & counter[i] %% b == 0) {
+  counter.output[i] = "FizzBuzz"     
+        }  
+    else if (counter[i] %% a == 0) {
+         counter.output[i] = "Fizz"
+      }
+    else if (counter[i] %% b == 0) {
+        counter.output[i] = "Buzz"
+      }
+    else counter.output[i] = counter[i]
+}
+
